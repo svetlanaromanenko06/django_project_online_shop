642d4a0d3041d903a21b1433ad9d93b8ec30501b
svetlanaromanenko06 <svroman07@gmail.com>
2023-08-09 22:15:42 +0300

    fix urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from catalog.views import shop_home, shop_contacts
 
+from catalog.apps import CatalogConfig
+
+
+app_name = CatalogConfig.name
+
 urlpatterns = [
     path('', shop_home),
     path('contacts/', shop_contacts)
