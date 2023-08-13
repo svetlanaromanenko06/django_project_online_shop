af15268bab123c1532107c75d21febda7ed3f689
svetlanaromanenko06 <svroman07@gmail.com>
2023-08-13 19:23:50 +0300

    fix databases

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_project_online_shop',
         'USER': 'postgres',
         'PASSWORD': '123123'
