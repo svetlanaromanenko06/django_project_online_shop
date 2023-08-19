152990d63c358dffa3443f4f3c7ead5ec7090db1
svetlana romanenko <svroman07@gmail.com>
2023-08-19 15:28:59 +0300

    fix admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -10,6 +10,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
+    list_display = ('id', 'name', 'price', 'category', 'preview')
     list_filter = ('category',)
     search_fields = ('name', 'description')
