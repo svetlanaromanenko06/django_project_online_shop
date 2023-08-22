7abdcf900770761548a1aa3f5e2a3ca4199ab1b6
svetlana romanenko <svroman07@gmail.com>
2023-08-22 23:52:18 +0300

    изменение контроллера и url

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
+
 #def shop_home(request):
 #    return render(request, 'catalog/shop_home.html')
-
 #def shop_contacts(request):
 #    return render(request, 'catalog/shop_contacts.html')
 
-def shop_home(request):
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/shop_home.html'
+
+'''def shop_home(request):
     """Контроллер для главной страницы"""
 
     product_list = Product.objects.all()
@@ -16,7 +22,7 @@ def shop_home(request):
                'title': 'Интерьерный салон Нефертити'}
 
     return render(request, 'catalog/shop_home.html', context=context)
-
+'''
 
 def shop_contacts(request):
     context = {
@@ -25,11 +31,20 @@ def shop_contacts(request):
     return render(request, 'catalog/shop_contacts.html', context=context)
 
 
-def discription(request):
+'''def discription(request):
 
     product_list = Product.objects.all()
     context = {'object_list': product_list,
                'title': 'Каталог товаров'}
 
     return render(request, 'catalog/discription.html', context=context)
+'''
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+
+
+
 
