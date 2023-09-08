8b370770d19d0c33afea2327d5910f62c0dc3f0e
svetlana romanenko <svroman07@gmail.com>
2023-09-08 23:09:25 +0300

    added view

--- catalog/views.py
+++ catalog/views.py
@@ -1,28 +1,32 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from catalog.models import Product
-
-
-#def shop_home(request):
-#    return render(request, 'catalog/shop_home.html')
-#def shop_contacts(request):
-#    return render(request, 'catalog/shop_contacts.html')
+from django.urls import reverse_lazy, reverse
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Version
 
 
 class ProductListView(ListView):
     model = Product
     template_name = 'catalog/shop_home.html'
 
-'''def shop_home(request):
-    """Контроллер для главной страницы"""
+    def get_context_data(self, **kwargs):
 
-    product_list = Product.objects.all()
-    context = {'object_list': product_list,
-               'title': 'Интерьерный салон Нефертити'}
+        context = super().get_context_data(**kwargs)
 
-    return render(request, 'catalog/shop_home.html', context=context)
-'''
+        for product in context['object_list']:
+            active_version = product.version_set.filter(is_active=True).first()
+            if active_version:
+                product.active_version_number = active_version.version_number
+                product.active_version_name = active_version.version_name
+            else:
+                product.active_version_number = None
+                product.active_version_name = None
+
+        return context
+
+
 
 def shop_contacts(request):
     context = {
@@ -30,21 +34,57 @@ def shop_contacts(request):
     }
     return render(request, 'catalog/shop_contacts.html', context=context)
 
-
-'''def discription(request):
-
-    product_list = Product.objects.all()
-    context = {'object_list': product_list,
-               'title': 'Каталог товаров'}
-
-    return render(request, 'catalog/discription.html', context=context)
-'''
-
-
 class ProductDetailView(DetailView):
     model = Product
 
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:shop_home')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    #success_url = reverse_lazy('catalog:product_list')
+
+    def get_success_url(self):
+        return reverse('catalog:update_product', args=[self.kwargs.get('pk')])
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+
+        return super().form_valid(form)
+
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            formset = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            formset = VersionFormset(instance=self.object)
+
+        context_data['formset'] = formset
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
 
 
 
-
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:shop_home')
